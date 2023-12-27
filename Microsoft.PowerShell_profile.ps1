fb950d381cef750b36ce1e9799013a0c35a0b901
Derek Erdmann <derek@derekerdmann.com>
2023-12-27 15:34:40 -0800

    Finish converting p10k prompt to oh-my-posh

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,4 @@
+Set-PSReadLineOption -PredictionSource None
+Set-PSReadlineOption -Colors @{ "Parameter"="Blue" }
+
 oh-my-posh init pwsh --config ~/dotfiles/oh-my-posh/powerlevel10k_custom.omp.json | Invoke-Expression

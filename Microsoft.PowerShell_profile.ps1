1c5396dfe69ba113ea8ded4da155f597c1193bcd
Derek Erdmann <derek@derekerdmann.com>
2023-12-27 17:47:44 -0800

    Override unreadable PowerShell terminal highlights

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,9 @@
 Set-PSReadLineOption -PredictionSource None
-Set-PSReadlineOption -Colors @{ "Parameter"="Blue" }
+
+# Override unreadable colors against a dark Solarized background
+Set-PSReadlineOption -Colors @{
+    "Parameter" = [ConsoleColor]::DarkBlue
+    "Operator" = [ConsoleColor]::Gray
+}
 
 oh-my-posh init pwsh --config ~/dotfiles/oh-my-posh/powerlevel10k_custom.omp.json | Invoke-Expression

a6211ead370cf8c4bdfe27201d6f787a83e1cf5e
Derek Erdmann <derek@derekerdmann.com>
2023-12-29 10:03:30 -0800

    Simplify PowerShell file highlighting

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,4 +6,8 @@ Set-PSReadlineOption -Colors @{
     "Operator" = [ConsoleColor]::Gray
 }
 
+$PSStyle.FileInfo.Directory = "$($PSStyle.Foreground.Cyan)"
+$PSStyle.FileInfo.SymbolicLink = "$($PSStyle.Foreground.Magenta)"
+$PSStyle.FileInfo.Executable = "$($PSStyle.Foreground.Green)"
+
 oh-my-posh init pwsh --config ~/dotfiles/oh-my-posh/powerlevel10k_custom.omp.json | Invoke-Expression
